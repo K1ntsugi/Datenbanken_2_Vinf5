2115102782bea017c54d0d009ebbb6c603857b27
Maximilian_Groetsch <max-groetsch@gmx.de>
2022-12-30 23:05:08 +0100

    wip ferien

--- PS-SQL/MySQL_Stored_Programs.sql
+++ PS-SQL/MySQL_Stored_Programs.sql
@@ -1,9 +1,29 @@
-DROP PROCEDURE if exists showCustomers;
+/*
+Database: Classic models
+Source: https://www.mysqltutorial.org/mysql-sample-database.aspx
+*/
 
+-- Testing a basic Stored Procedure
+USE classicmodels;
+DROP PROCEDURE if exists showCustomers;
 DELIMITER $$
 CREATE PROCEDURE showCustomers()
 BEGIN
     SELECT * FROM customers WHERE 1;
 END $$
 DELIMITER ;
-/***********************************************/
+
+-- Testing a basic stored Function
+DROP FUNCTION if exists isRich;
+
+DELIMITER $$
+CREATE FUNCTION isRich(credit INT) returns bool
+    DETERMINISTIC
+BEGIN
+    IF(credit >= 200000) THEN
+        return true;
+    ELSE
+        return false;
+    END IF;
+END $$
+DELIMITER ;
